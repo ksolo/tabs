9b703b652702bd7948e68d322fc8d0883894f9bc
Kevin Solorio <soloriok@gmail.com>
2013-09-27 15:52:26 -0500

    tabs problem

--- tabs.js
+++ tabs.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
   // Put your JavaScript behavior here
+  $('#tab1').show();
+
+  $('.tabs li').click(function(event){
+    event.preventDefault();
+
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+
+    var divId = $(this).find('a').attr('href');
+    $('.tab').hide();
+    $(divId).show();
+
+  });
 });
